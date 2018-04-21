c74aaaa485d3bcdbc9765d37100969f23c202908
Rolf van Kleef <rolf@vankleef.me>
2018-04-21 14:41:50 +0200

    Example of buildings and cards structure

---
+++ scripts/buildings.lua
@@ -0,0 +1,31 @@
+buildings = buildings or {}
+
+buildings.small_generator = {
+    name = "Generator",
+    size = { x = 1, z = 1 },
+    asset = "small_generator" -- Autoloads generator asset from models
+}
+
+buildings.small_office = {
+    name = "Office building",
+    size = { x = 1, z = 1 },
+    asset = "small_office",
+}
+
+buildings.large_office = {
+    name = "Headquarters",
+    size = { x = 2, z = 2 },
+    asset = "large_office",
+}
+
+buildings.small_residential = {
+    name = "Small house",
+    size = { x = 1, z = 1 },
+    asset = "small_residential",
+}
+
+buildings.medium_residential = {
+    name = "Appartment building",
+    size = { x = 1, z = 1 },
+    asset = "medium_residential",
+}
---
+++ scripts/cards.lua
@@ -0,0 +1,87 @@
+cards = cards or {}
+
+cards.generator = {
+    name = "Generator",
+    effects = {
+        {
+            type = "add_card",
+            card = "nuclear_generator", -- Autoloads id from `cards`
+        },
+        {
+            type = "add_card",
+            card = "wind_generator",
+        },
+        {
+            type = "place_building",
+            building = "small_generator",
+        }
+    },
+    requirements = {},
+    costs = {},
+    is_creeper = false,
+}
+
+cards.small_office = {
+    name = "Office building",
+    effects = {
+        {
+            type = "add_card",
+            card = "large_office", -- Autoloads from `cards`
+        },
+        {
+            type = "place_building",
+            building = "small_office",
+        }
+    },
+    requirements = {},
+    costs = {},
+    is_creeper = false,
+}
+
+local function verifyResourceRequirement(requirement, state)
+    if requirement.relation == "gt" then
+        return state.properties[requirement.property] > requirement.value
+    end
+    if requirement.relation == "lt" then
+        return state.properties[requirement.property] < requirement.value
+    end
+    if requirement.relation == "gte" then
+        return state.properties[requirement.property] >= requirement.value
+    end
+    if requirement.relation == "lte" then
+        return state.properties[requirement.property] <= requirement.value
+    end
+    if requirement.relation == "eq" then
+        return state.properties[requirement.property] == requirement.value
+    end
+end
+
+for _, card in pairs(cards) do
+    function card:verifyRequirements(state)
+        for _, requirement in ipairs(self.requirements) do
+            if requirement.type == "resource" then
+                if not verifyResourceRequirement(requirement, state) then return false end
+            end
+        end
+        return true
+    end
+    function card:applyCosts(state)
+        for _, cost in ipairs(self.costs) do
+            state.properties[cost.property] = state.properties[cost.property] + cost.value
+        end
+    end
+    function card:runEffects(state)
+        for _, effect in ipairs(self.effects) do
+            if effect.type == "add_card" then
+
+            elseif effect.type == "place_building" then
+
+            end
+        end
+    end
+    function card:run(state)
+        if not self:verifyRequirements(state) then return false end
+        self:applyCosts(state)
+        self:runEffects(state)
+    end
+end
