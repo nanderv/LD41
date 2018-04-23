f03d33e6808b33a8ec541d9e48b675f44a90ddc7
Nander Voortman <nander@nander.net>
2018-04-23 21:06:24 +0200

    Fixes creepers.

--- scripts/states/showCreepers.lua
+++ scripts/states/showCreepers.lua
@@ -5,27 +5,34 @@ function menu:enter(prev, cards)
 end
 
 local offsets = {
-    { x = 621, y = 153},
-    { x = 373, y = 153},
-    { x = 869, y = 153},
-    { x = 124, y = 153},
-    { x = 1118, y = 153},
+    { x = 621, y = 153 },
+    { x = 373, y = 153 },
+    { x = 869, y = 153 },
+    { x = 124, y = 153 },
+    { x = 1118, y = 153 },
 
-    { x = 621, y = 460},
-    { x = 373, y = 460},
-    { x = 869, y = 460},
-    { x = 124, y = 460},
-    { x = 1118, y = 460},
+    { x = 621, y = 460 },
+    { x = 373, y = 460 },
+    { x = 869, y = 460 },
+    { x = 124, y = 460 },
+    { x = 1118, y = 460 },
 }
 
 function menu:draw()
     menu.prev:draw()
-    for i in range(math.min(#offsets, #menu.cards)) do
+    for i = 1, math.min(#offsets, #menu.cards) do
         scripts.rendering.renderCard.renderCard(menu.cards[i], offsets[i].x, offsets[i].y, 0.5)
     end
 end
 
 function menu:mousepressed(x, y, click)
+    for i = 1, math.min(#offsets, #menu.cards) do
+        for k, card in pairs(scripts.gameobjects.cards) do
+            if menu.cards[i] == card then
+                STATE.discardPile[#STATE.discardPile + 1] = k
+            end
+        end
+    end
     Gamestate.pop()
 end
 
